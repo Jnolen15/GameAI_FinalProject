e6a94a46e89146937115714fcf874a3664920f3a
Jared Nolen <Legodoghead@gmail.com>
2020-11-27 15:37:37 -0800

    Added random name generation stuff

--- Species.py
+++ Species.py
@@ -1,9 +1,10 @@
+import random
 
 # Species Class
 class species:
     # A Species Stats
     def __init__(self, offense=0, defense=0, heatRes=0, \
-                coldRes=0, social=0, size=0, diet=0, swim=0, walk=0, fly=0):
+                coldRes=0, social=0, size=0, diet=0, swim=0, walk=0, fly=0, name="", meaning=""):
         # BASIC STATS
         # Lowest stat score = 0, highest = 99
         self.offense = offense
@@ -20,6 +21,11 @@ class species:
         self.swim = swim
         self.walk = walk
         self.fly = fly
+
+        # Name
+        nameTuple = nameSelect()
+        self.name = nameTuple[0]
+        self.meaning = nameTuple[1]
 
     # Print functions
     def __repr__(self):
@@ -57,9 +63,39 @@ class species:
         if self.fly > 50:
             moveTypes.append("Fly")
 
-        return "\n~~~~~Stats~~~~~ \nOffense: %s \nDefense: %s \
+        return "\n~~~~~Stats~~~~~ \nName: %s \nMeaning: %s \nOffense: %s \nDefense: %s \
                 \nHeatRes: %s \nColdRes: %s \nSocial: %s \nSize: %s \nDiet: %s \
                 \nMovement: %s" \
-                % (self.offense, self.defense, self.heatRes, \
+                % (self.name, self.meaning, self.offense, self.defense, self.heatRes, \
                 self.coldRes, self.social, sizeType, dietType, moveTypes)
 
+def nameSelect():
+    # Dictionary of roots + meanings
+    prefix = {
+        "elasmo" : "metal plated",
+        "thallasso" : "sea",
+        "archaeo" : "ancient",
+        "micro" : "small",
+    }
+    
+    suffix = {
+        "tops" : "face",
+        "saur" : "lizard",
+        "dactyl" : "reptile",
+        "rex" : "king",
+    }
+    randName = ""
+    nameDef = ""
+    # Choosing Prefix
+    pre = random.choice(list(prefix.keys()))
+    randName += pre
+    nameDef += prefix.get(pre) + " "
+    # Choosing Suffix
+    suf = random.choice(list(suffix.keys()))
+    randName += suf
+    nameDef += suffix.get(suf)
+    #for i in range(2):
+    #    root = random.choice(list(roots.keys()))
+    #    randName += root
+    #    nameDef += roots.get(root) + " "
+    return (randName, nameDef)
