4952fc7e159dfff4e80dde9955ca1032d53a5fce
Darcy Phipps <diphipps@ucsc.edu>
2020-12-05 13:10:31 -0800

    Made basic json

--- goap.py
+++ goap.py
@@ -5,19 +5,43 @@ import json
 #So that GOAP doesn't have to deal with slightly missing decimals
 #Going to import functions from a JSON file like in p5
 
+
+#Initially define the JSON to be false so that we can initialise it properly
+Explanations = False
+
 #Going off of how P5 did it:
 
 #Takes JSON objects/dictionaries and turns them into functions
-def makeChange(change):
+def makeChange(changes):
     def effect(state):
         next_state = state.copy()
-        for attribute, change in state["Changes"]:
+        for attribute, change in changes["Changes"]:
             next_state[attribute] += change
+        
+        return next_state
 
     return effect
 
+# Should really make restrictions too
+def makeRequirement(changes):
+    def requirement(state):
+        for attribute, requirement in changes["Requirements"]:
+            if requirement[1] == "Equal":
+                if state[attribute] != requirement:
+                    return False
+
+            if requirement[1] == "Less than":
+                if state[attribute] <= requirement:
+                    return False
+            if requirement[1] == "Greater than":
+                if state[attribute] >= requirement:
+                    return False
+        return True
+
+    return requirement
 #Only want to initialise the possible changes once, so function for it
 def init():
+    global Explanations
     with open("GOAP_Actions.json") as f:
         Explanations = json.load(f)
-        #Need to ask in office hours why in P5 we could access the equivalent variable anywhere
+    #Really hope this is correct global syntax
